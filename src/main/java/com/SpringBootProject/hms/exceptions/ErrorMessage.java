eed3fb42f5186245a7b0a5050447031a59e082c1
Abiral07 <abiralkhadka7@gmail.com>
2022-07-25 11:11:44 +0545

    Bean Validation Error Binding

--- src/main/java/com/SpringBootProject/hms/exceptions/ErrorMessage.java
+++ src/main/java/com/SpringBootProject/hms/exceptions/ErrorMessage.java
@@ -1,15 +1,23 @@
 package com.SpringBootProject.hms.exceptions;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ErrorMessage {
+    private LocalDateTime timestamp;
     private HttpStatus status;
+    private Integer statusValue;
     private Throwable cause;
+    private String error;
     private String message;
+    private String path;
 }
--- src/main/java/com/SpringBootProject/hms/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/SpringBootProject/hms/exceptions/RestResponseEntityExceptionHandler.java
@@ -1,21 +1,47 @@
 package com.SpringBootProject.hms.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorMessage> CustomException(CustomException customException, WebRequest request){
-        ErrorMessage message = new ErrorMessage(HttpStatus.BAD_REQUEST, customException.getCause(),customException.getMessage());
+    public ResponseEntity<Object> CustomException(CustomException customException, WebRequest request) {
+        return this.createResponseEntity(HttpStatus.NOT_ACCEPTABLE, customException, request);
+    }
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        //to extract the default error message from diagnostic information about the errors held in MethodArgumentNotValidException
+        Exception exception = new Exception(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return this.createResponseEntity(HttpStatus.BAD_REQUEST, exception, request);
+    }
+
+    private ResponseEntity<Object> createResponseEntity(
+            HttpStatus httpStatus, Exception ex, WebRequest request) {
+        ErrorMessage errorResponse = ErrorMessage.builder()
+                .timestamp(LocalDateTime.now())
+                .status(httpStatus)
+                .statusValue(httpStatus.value())
+                .error(httpStatus.getReasonPhrase())
+                .message(ex.getMessage())
+                .path(request.getDescription(true))
+                .build();
+        return handleExceptionInternal(ex, errorResponse,
+                new HttpHeaders(), httpStatus, request);
     }
 }
+
